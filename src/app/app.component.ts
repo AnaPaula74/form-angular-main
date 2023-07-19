10f8d32e0ec11b3acbd59893a0074209e5709e37
Ana Paula Mendonça Lima <123080363+AnaPaula74@users.noreply.github.com>
2023-07-19 18:40:19 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'life-cycle-projeto-inicial';
+  title = 'forms-template';
 }
